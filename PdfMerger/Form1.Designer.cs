378ae2e65f9c151674b312be799accc188452d37
NicolasAgustin <nickrak10@gmail.com>
2023-07-11 23:58:52 -0300

    Agregar archivos de proyecto.

---
+++ PdfMerger/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PdfMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combinar = new System.Windows.Forms.Button();
+            this.displayFiles = new System.Windows.Forms.CheckedListBox();
+            this.seleccionar = new System.Windows.Forms.Button();
+            this.filenameCheckbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // combinar
+            // 
+            this.combinar.Location = new System.Drawing.Point(267, 369);
+            this.combinar.Name = "combinar";
+            this.combinar.Size = new System.Drawing.Size(280, 69);
+            this.combinar.TabIndex = 0;
+            this.combinar.Text = "COMBINAR";
+            this.combinar.UseVisualStyleBackColor = true;
+            this.combinar.Click += new System.EventHandler(this.combinar_Click);
+            // 
+            // displayFiles
+            // 
+            this.displayFiles.FormattingEnabled = true;
+            this.displayFiles.Location = new System.Drawing.Point(12, 12);
+            this.displayFiles.Name = "displayFiles";
+            this.displayFiles.Size = new System.Drawing.Size(172, 274);
+            this.displayFiles.TabIndex = 1;
+            // 
+            // seleccionar
+            // 
+            this.seleccionar.Location = new System.Drawing.Point(12, 292);
+            this.seleccionar.Name = "seleccionar";
+            this.seleccionar.Size = new System.Drawing.Size(172, 37);
+            this.seleccionar.TabIndex = 2;
+            this.seleccionar.Text = "SELECCIONAR ARCHIVOS";
+            this.seleccionar.UseVisualStyleBackColor = true;
+            this.seleccionar.Click += new System.EventHandler(this.seleccionar_Click);
+            // 
+            // filenameCheckbox
+            // 
+            this.filenameCheckbox.Location = new System.Drawing.Point(267, 225);
+            this.filenameCheckbox.Name = "filenameCheckbox";
+            this.filenameCheckbox.Size = new System.Drawing.Size(280, 20);
+            this.filenameCheckbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "NOMBRE DEL ARCHIVO (SIN .PDF)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filenameCheckbox);
+            this.Controls.Add(this.seleccionar);
+            this.Controls.Add(this.displayFiles);
+            this.Controls.Add(this.combinar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button combinar;
+        private System.Windows.Forms.CheckedListBox displayFiles;
+        private System.Windows.Forms.Button seleccionar;
+        private System.Windows.Forms.TextBox filenameCheckbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PdfMerger/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PdfMerger
+{
+    public partial class Form1 : Form
+    {
+        private List<string> filesToMerge = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void combinar_Click(object sender, EventArgs e)
+        {
+            string folder = "";
+            string filename = "";
+
+            if (this.filesToMerge == null) { return; }
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                folder = dialog.SelectedPath;
+            } else
+            {
+                // TODO: mostrar mensaje de error
+                return;
+            }
+
+            if (this.filenameCheckbox.Text == string.Empty)
+            {
+                filename = "output.pdf";
+            } else
+            {
+                filename = this.filenameCheckbox.Text + ".pdf";
+            }
+
+            PdfDocument document = new PdfDocument();
+
+            foreach(var pdf in this.filesToMerge)
+            {
+
+                FileInfo fi = new FileInfo(pdf);
+                if (fi.Extension == ".pdf")
+                {
+                    using (var pdfReaded = PdfReader.Open(pdf, PdfDocumentOpenMode.Import))
+                    {
+                        for (int i = 0; i < pdfReaded.PageCount; i++)
+                        {
+                            document.AddPage(pdfReaded.Pages[i]);
+                        }
+                    }
+                } else
+                {
+                    try
+                    {
+                        // TODO: Redimensionar foto para que encaje perfectamente en A4
+                        PdfPage page = document.AddPage();
+                        XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                        XImage image = XImage.FromFile(pdf);
+
+                        page.Width = image.PixelWidth;
+                        page.Height = image.PixelHeight;
+                        gfx.DrawImage(image, 0, 0, image.PixelWidth, image.PixelHeight);
+                    } catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+
+
+            }
+
+            string finalpath = Path.Combine(folder, filename);
+
+            document.Save(finalpath);
+
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+            MessageBox.Show("Se creo el archivo " + finalpath, "Pdf creado", buttons);
+
+            clean_form();
+        }
+
+        private void clean_form()
+        {
+            this.filesToMerge = null;
+            this.filenameCheckbox.Text = string.Empty;
+            this.displayFiles.Items.Clear();
+        }
+
+        private void seleccionar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "All Files (*.*)|*.*";
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+               this.filesToMerge = dialog.FileNames.ToList();
+               foreach(var f in this.filesToMerge)
+               {
+                    this.displayFiles.Items.Add(f);
+               }
+            }
+
+        }
+    }
+}
